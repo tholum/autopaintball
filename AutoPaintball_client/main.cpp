3be14b0386652ba11c7ae05f0bb5ac64027fab32
tholum <tholum@timslaptop>
2013-05-12 21:02:21 -0500

    Adding Files

---
+++ AutoPaintball_client/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: tholum
+ *
+ * Created on May 12, 2013, 9:01 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
