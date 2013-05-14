68e10e759264ddd1dc0b2f21e7d4cbb0d1600b6e
tim.holum <webbible@couleetechlink.com>
2013-05-13 20:15:20 -0500

    Initial Build

--- AutoPaintball_client/main.cpp
+++ AutoPaintball_client/main.cpp
@@ -5,15 +5,21 @@
  * Created on May 12, 2013, 9:01 PM
  */
 
-#include <cstdlib>
 
-using namespace std;
+#include <cstdlib>
+#include <cv.h>
+using namespace cv;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-
+    CvCapture* capture = cvCaptureFromCAM( CV_CAP_ANY );
+    while(1){
+        IplImage* frame = cvQueryFrame( capture );
+        imwrite( "/var/www/image.jpg", frame );
+        
+    }
     return 0;
 }
 
